11ba9bfe986fab6a616979e98080b5ad7791bb98
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-01-31 16:51:28 +0100

    Fixed threshold and similarity
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- recommendation-system-engine/src/app.py
+++ recommendation-system-engine/src/app.py
@@ -32,7 +32,6 @@ def recommendations(user_id):
     results = compute_recommendations(user_id, features)
     events = list()
     for event in results['events']:
-        current_app.logger.info(event[0])
         events.append(mongodb_utils.get_events_collection(mongo).find_one({'_id': ObjectId(event[0])}))
     results['events'] = events
     return dumps(results)
@@ -42,7 +41,7 @@ def compute_recommendations(target_user_id, features):
     users_list = list(mongodb_utils.get_users_collection(mongo).find({'_id': {'$ne': ObjectId(target_user_id)}}))
     similar_users_list = list()
     events_id_to_suggest = dict()
-    initial_threshold = 0.02
+    initial_threshold = 0.2
     threshold = compute_threshold(initial_threshold, features)
     for user in users_list:
         target_user_events = [e['event_id'] for e in sqlite_utils.query_db('SELECT event_id FROM user_event WHERE user_id = ?', (target_user_id,)) if 'event_id' in e and e['event_id'] != 'None']
@@ -95,7 +94,8 @@ def users_similarity(target_user, other_user, target_user_events, other_user_eve
         target_user_vect.append((1 * target_user_category_weight))
         other_user_vect.append((1 * other_user_category_weight))
     alpha = len(common_events) / len(target_user_events)
-    return alpha * spatial.distance.cosine(target_user_vect, other_user_vect)
+    consine_similarity = (-1) * (spatial.distance.cosine(target_user_vect, other_user_vect) - 1)
+    return alpha * consine_similarity
 
 def compute_beta(target_user_age, other_user_age):
     d = abs(target_user_age - other_user_age)
